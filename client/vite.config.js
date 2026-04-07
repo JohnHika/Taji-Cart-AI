0657fd2b5bd85613dd827af5e94eb4bc267a867a
Ivy-NW <ivynjoroge30@gmail.com>
2026-04-07 08:43:13 +0300

    ui changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BACKEND = process.env.VITE_BACKEND_TARGET || 'http://localhost:3001'
+const BACKEND = process.env.VITE_BACKEND_TARGET || 'http://localhost:5000'
 
 // https://vitejs.dev/config/
 export default defineConfig({
