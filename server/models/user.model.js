88c6014584d35b5de00859dce675b0e17f0e3ba1
John Hika <johnkimani576@gmail.com>
2026-04-08 15:02:58 +0300

    fix: make password optional for Google OAuth users

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     password : {
         type : String,
-        required : [true, "provide password"]
+        required : function() { return !this.googleId; }
     },
     avatar : {
         type : String,
